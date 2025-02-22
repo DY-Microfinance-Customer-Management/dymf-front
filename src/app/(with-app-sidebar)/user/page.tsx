74a2db566f0c5559d4d59e84e7def55c853b1132
후니 <hipark@huniui-MacBookPro.local>
2025-02-22 15:34:51 +0900

    Feat: User Registration

--- src/app/(with-app-sidebar)/user/page.tsx
+++ src/app/(with-app-sidebar)/user/page.tsx
@@ -1,36 +1,65 @@
 'use client';
 
+// Actions
+import { useActionState, useEffect } from "react";
+
 // UI Components
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { PasswordInput } from "@/components/ui/password-input";
+import { toast } from "sonner";
+
+// React
+import { createUserAction } from "@/actions/create-user.action";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+    // Router
+    const router = useRouter();
+    
+    // Server Action
+    const [state, formAction, isPending] = useActionState(createUserAction, null);
+    useEffect(() => {
+        if (state === null) return;
+
+        if (state?.status === 200) {
+            toast.success(`User has been successfully registered!`);
+        } else if (state?.status === 401 || 403) {
+            toast.error(state?.message);
+            router.push('/login');
+        } else {
+            toast.error(state?.message);
+        }
+    }, [state]);
+
     return (
         <div className="flex flex-col p-10 space-y-8 min-h-screen">
             <h1 className="text-3xl font-bold">User Management</h1>
 
             <div className="flex justify-between space-x-8">
                 <Card className="w-1/3">
-                    <CardHeader>
-                        <CardTitle className="text-green-800">Add Users</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <div className="space-y-4">
-                            <div className="flex flex-col space-y-1">
-                                <Label>Principal</Label>
-                                <Input className="w-full" type="number" />
+                    <form action={formAction}>
+                        <CardHeader>
+                            <CardTitle className="text-green-800">Add Users</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="space-y-4">
+                                <div className="flex flex-col space-y-1">
+                                    <Label>Username</Label>
+                                    <Input name="username" disabled={isPending} className="w-full" type="text" />
+                                </div>
+                                <div className="flex flex-col space-y-1">
+                                    <Label>Password</Label>
+                                    <PasswordInput name="password" disabled={isPending} className="w-full" />
+                                </div>
                             </div>
-                            <div className="flex flex-col space-y-1">
-                                <Label>Interest Rate (%)</Label>
-                                <Input className="w-full" type="number" />
-                            </div>
-                        </div>
-                    </CardContent>
-                    <CardFooter>
-                        <Button className="w-full bg-blue-600 hover:bg-blue-700 text-white">Add</Button>
-                    </CardFooter>
+                        </CardContent>
+                        <CardFooter>
+                            <Button type="submit" className="w-full bg-blue-600 hover:bg-blue-700 text-white">Confirm</Button>
+                        </CardFooter>
+                    </form>
                 </Card>
 
                 <Card className="w-2/3">
