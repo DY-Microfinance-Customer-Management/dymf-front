9a22c443b04a579dc6d53f285ed8aafcb8630431
후니 <hipark@huniui-MacBookPro.local>
2025-03-29 15:08:40 +0900

    Feat: Home Page

--- src/app/(with-app-sidebar)/home/page.tsx
+++ src/app/(with-app-sidebar)/home/page.tsx
@@ -1,7 +1,20 @@
+"use client"
+
+import Image from "next/image"
+
 export default function Page() {
     return (
-        <div>
-            Home
+        <div className="flex flex-col justify-center items-center h-screen space-y-6">
+            <h1 className="text-4xl font-bold text-gray-700">Welcome to</h1>
+            <Image
+                src="/dymf_logo.png"
+                width={300}
+                height={0}
+                alt="DYMF Logo"
+                className="object-contain"
+                priority
+            />
+            <h1 className="text-4xl font-semi-bold text-gray-700">Loan Management System</h1>
         </div>
     );
 }
