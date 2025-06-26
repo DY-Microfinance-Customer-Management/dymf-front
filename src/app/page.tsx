8a941b481c7758117cd8700e9367b3ea69cd5114
후니 <hipark@huniui-MacBookPro.local>
2025-06-26 20:39:17 +0900

    Refactor: Home page redirection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+'use client';
+
+// Components
+import Image from "next/image";
+
+// React
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+// Utils
+import { delay } from "@/util/delay";
+
 export default function Home() {
+    const router = useRouter();
+
+    useEffect(() => {
+        delay(3000);
+        router.push('/home');
+    }, []);
+
     return (
-        <div>
-            Hello
+        <div className="h-screen w-screen flex items-center justify-center">
+            <Image
+                src="/dymf_logo.png"
+                alt="DYM Finance Logo"
+                width={500}
+                height={200}
+                priority
+            />
         </div>
     );
 }
