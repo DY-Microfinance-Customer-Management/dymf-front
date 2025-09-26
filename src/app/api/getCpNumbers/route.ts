f9f1eb20e1f16aef6d4ec2d25a2d3d67a1ff0ac7
후니 <hipark@huniui-MacBookPro.local>
2025-09-27 01:42:17 +0900

    Fix: CP No. Registration Error

--- src/app/api/getCpNumbers/route.ts
+++ src/app/api/getCpNumbers/route.ts
@@ -1,23 +1,34 @@
 import { cookies } from 'next/headers';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(req: Request) {
+export async function GET(request: NextRequest) {
     const cookieStore = await cookies();
     const credentials = cookieStore.get('access_token')?.value;
 
-    const { searchParams } = new URL(req.url);
-    const cursor = searchParams.get('cursor') ?? '';
+    const searchParams = request.nextUrl.searchParams;
+    const nextCursor = searchParams.get('cursor');
     const order = searchParams.getAll('order[]');
     const goBack = searchParams.get('goBack') ?? 'false';
     const take = searchParams.get('take') ?? '20';
 
-    const query = new URLSearchParams();
-    if (cursor) query.append('cursor', cursor);
-    order.forEach(o => query.append('order[]', o));
-    query.append('goBack', goBack);
-    query.append('take', take);
+    let apiUrl = `${process.env.API_SERVER_URL}/personnel/cpnumber`;
 
-    const response = await fetch(`${process.env.API_SERVER_URL}/personnel/cpnumber?${query.toString()}`, {
+    if (nextCursor) {
+        apiUrl += `?cursor=${encodeURIComponent(nextCursor)}`;
+    } else {
+        const query = new URLSearchParams();
+        if (order.length > 0) {
+            order.forEach(o => query.append('order[]', o));
+        } else {
+            query.append('order[]', 'id_ASC');
+        }
+        query.append('goBack', goBack);
+        query.append('take', take);
+
+        apiUrl += `?${query.toString()}`;
+    }
+
+    const response = await fetch(apiUrl, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
