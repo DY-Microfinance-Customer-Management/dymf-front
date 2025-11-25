99709dd3d68ac542b393b150f690f781438966d4
후니 <hipark@huniui-MacBookPro.local>
2025-11-25 16:36:04 +0900

    Test: LoanType Save Error

--- src/actions/create-customer.action.ts
+++ src/actions/create-customer.action.ts
@@ -33,85 +33,85 @@ export async function createCustomerAction(_: any, formData: FormData): Promise<
 
     console.log(`Customer Registration Data: ${JSON.stringify(data)}`)
 
-    // const file = formData.get('image') as File;
-    // if (file.size !== 0) {
+    const file = formData.get('image') as File;
+    if (file.size !== 0) {
 
-    //     if (file && file instanceof File) {
-    //         const response = await fetch(`${process.env.API_SERVER_URL}/common`, {
-    //             method: 'POST',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //                 'Authorization': `Bearer ${credentials}`
-    //             },
-    //         })
-    //         const responseData = await response.json();
-    //         const image_address = responseData.url;
+        if (file && file instanceof File) {
+            const response = await fetch(`${process.env.API_SERVER_URL}/common`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${credentials}`
+                },
+            })
+            const responseData = await response.json();
+            const image_address = responseData.url;
             
-    //         const extractFileName = (image_address: string): string | null => {
-    //             const regex = /\/([^\/?]+\.jpg)/;
-    //             const match = image_address.match(regex)
-    //             return match ? match[1] : null
-    //         }
+            const extractFileName = (image_address: string): string | null => {
+                const regex = /\/([^\/?]+\.jpg)/;
+                const match = image_address.match(regex)
+                return match ? match[1] : null
+            }
             
-    //         const imageName = extractFileName(String(image_address));
+            const imageName = extractFileName(String(image_address));
             
-    //         if (imageName) {
-    //             data.image = imageName;
+            if (imageName) {
+                data.image = imageName;
                 
-    //             await fetch(`${image_address}`, {
-    //                 method: 'PUT',
-    //                 body: file,
-    //                 headers: {
-    //                     'Content-Type': file.type
-    //                 }
-    //             })
-    //         }
+                await fetch(`${image_address}`, {
+                    method: 'PUT',
+                    body: file,
+                    headers: {
+                        'Content-Type': file.type
+                    }
+                })
+            }
 
-    //     }
-    // }
+        }
+    }
     
-    // const response = await fetch(`${process.env.API_SERVER_URL}/customer`, {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //         'Authorization': `Bearer ${credentials}`
-    //     },
-    //     body: JSON.stringify(data), // 1MB 넘어가면 Error
-    // });
+    const response = await fetch(`${process.env.API_SERVER_URL}/customer`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${credentials}`
+        },
+        body: JSON.stringify(data), // 1MB 넘어가면 Error
+    });
 
-    // if (!response.ok) {
-    //     const status = response.status;
+    if (!response.ok) {
+        const status = response.status;
 
-    //     if (status === 400) {
-    //         return {
-    //             status: 400,
-    //             message: 'Something went wrong :( Please check the values of the customer information.'
-    //         }
-    //     } else if (status === 409) {
-    //         return {
-    //             status: 409,
-    //             message: 'Customer already exists! Please check again.'
-    //         }
-    //     } else if (status === 401 || 403) {
-    //         return {
-    //             status: status,
-    //             message: 'Unauthorized request. Please Login again.'
-    //         }
-    //     } else if (status === 404) {
-    //         return {
-    //             status: status,
-    //             message: 'Not Found: 404'
-    //         }
-    //     }
-    // }
-
-    // return {
-    //     status: 200,
-    //     message: 'Customer successfully registered.'
-    // };
+        if (status === 400) {
+            return {
+                status: 400,
+                message: 'Something went wrong :( Please check the values of the customer information.'
+            }
+        } else if (status === 409) {
+            return {
+                status: 409,
+                message: 'Customer already exists! Please check again.'
+            }
+        } else if (status === 401 || 403) {
+            return {
+                status: status,
+                message: 'Unauthorized request. Please Login again.'
+            }
+        } else if (status === 404) {
+            return {
+                status: status,
+                message: 'Not Found: 404'
+            }
+        }
+    }
 
     return {
-        status: 999,
-        message: `${JSON.stringify(data)}`
-    }
+        status: 200,
+        message: 'Customer successfully registered.'
+    };
+
+    // return {
+    //     status: 999,
+    //     message: `${JSON.stringify(data)}`
+    // }
 }
